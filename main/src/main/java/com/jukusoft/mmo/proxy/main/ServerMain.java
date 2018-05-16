f0d1135c70ac1989ee5962b446b43a34e8ba4594
JuKu <kuenzel.justin@t-online.de>
2018-05-17 00:04:25 +0200

    added some maven modules.

---
+++ main/src/main/java/com/jukusoft/mmo/proxy/main/ServerMain.java
@@ -0,0 +1,9 @@
+package com.jukusoft.mmo.proxy.main;
+
+public class ServerMain {
+
+    public static void main (String[] args) {
+        System.out.println("========= Proxy Server ========");
+    }
+
+}
