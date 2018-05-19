ce4cf3118b50c4d5ae41ad84a04f7e8f8264e2f9
JuKu <kuenzel.justin@t-online.de>
2018-05-19 16:54:07 +0200

    changed database schema, added table.

--- database/src/main/resources/db/migration/V0001__create_schema.sql
+++ database/src/main/resources/db/migration/V0001__create_schema.sql
@@ -40,20 +40,24 @@ ALTER TABLE `${prefix}characters`
 ALTER TABLE `${prefix}characters`
 MODIFY `cid` int(10) NOT NULL AUTO_INCREMENT;
 
+--
+-- Tabellenstruktur für Tabelle `mmo_races`
+--
+
 CREATE TABLE IF NOT EXISTS `${prefix}races` (
 `id` int(10) NOT NULL,
-  `title` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indizes der exportierten Tabellen
---
+  `title` varchar(255) NOT NULL,
+  `start_sectorID` int(10) NOT NULL DEFAULT '1',
+  `selectable` int(10) NOT NULL DEFAULT '1',
+  `start_pos_x` float(10,4) NOT NULL,
+  `start_pos_y` float(10,4) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 --
 -- Indizes für die Tabelle `mmo_races`
 --
-ALTER TABLE `${prefix}races`
- ADD PRIMARY KEY (`id`);
+ALTER TABLE `vraces`
+ ADD PRIMARY KEY (`id`), ADD KEY `selectable` (`selectable`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
--- database/src/main/resources/db/migration/V0002__insert_example_sectors.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `${prefix}sectors` (
-    `sectorID`, `title`, `map_info_path`, `max_players`
-) VALUES (
-    '1', 'Dev Room', 'dev_room.tmx', '200'
-);
